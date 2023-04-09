3fba00d8338dedadb7e78e52a95a2c286ba6cd5c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-09 18:25:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
-import Navbar from './Navbar';
+import React from "react";
+import "../styles/App.css";
+import Navbar from "./Navbar";
 import { Routes, Route } from "react-router-dom";
-import Home from './Home';
-import Top from './Top';
-import Contact from './Contact';
+import Home from "./Home";
+import Top from "./Top";
+import Contact from "./Contact";
 
 const App = () => {
-
   return (
     <div id="main">
       <Navbar />
-      <div className='container'></div>
-    </div>
-  )
-}
 
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top-10" element={<Top />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
